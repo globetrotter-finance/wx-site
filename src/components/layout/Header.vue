cbd9872b02adfe23b8846884a902f2ad744e5b6b
devansh <devanshksg@gmail.com>
2025-06-16 12:26:57 +0530

    added drawer for small devices

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,55 +1,104 @@
 <template>
-<header class="fixed transform w-[100%] from-blue-700 to-blue-400 z-50 shadow-md rounded-2xls border border-gray-200">
-  <el-row class="container mx-auto flex items-center justify-between">
+  <header class="fixed transform w-[100%] from-blue-700 to-blue-400 z-50 shadow-md rounded-2xls border border-gray-200">
+    <el-row class="container mx-auto flex items-center justify-between">
 
-    <!-- Logo -->
-    <el-col class="flex" :span="4">
-      <el-link @click="router.push('/')" :underline="false">
-        <img src="../../assets/LOGO.png" alt="Saras" style="height: 40px; width: auto;" class="p-1"/>
-        <span class="text-xl bg-gradient-to-r from-blue-700 to-blue-400 bg-clip-text text-transparent">Saras Finance</span>
-      </el-link>
-    </el-col>
+      <!-- Logo -->
+      <el-col class="flex" :span="4">
+        <el-link @click="router.push('/')" :underline="false">
+          <img src="../../assets/LOGO.png" alt="Saras" style="height: 40px; width: auto;" class="p-1" />
+          <span
+            class="text-xl bg-gradient-to-r from-blue-700 to-blue-400 bg-clip-text text-transparent whitespace-nowrap">Saras
+            Finance</span>
+        </el-link>
+      </el-col>
 
-    <!-- App and Contacts -->
-    <div class="hidden md:flex items-center space-x-3 flex-shrink-0">
-      <el-dropdown>
-        <template #default>
-          <el-button type="primary" round text>Products</el-button>
+      <!-- Desktop Menu -->
+      <div class="hidden md:flex items-center space-x-3 flex-shrink-0">
+        <el-dropdown>
+          <template #default>
+            <el-button type="primary" round text>Products</el-button>
+          </template>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item @click="router.push('/compliance-governance')">Compliance, Risk &
+                Governance</el-dropdown-item>
+              <el-dropdown-item @click="router.push('/audit-automation')">Audit Automation</el-dropdown-item>
+              <el-dropdown-item @click="router.push('/knowledge-advisory')">Knowledge & Advisory</el-dropdown-item>
+              <el-dropdown-item @click="router.push('/litigation-management')">Litigation Management</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+        <el-button @click="takeMeToApp" type="success" round>Go to Saras Portal</el-button>
+        <el-button @click="router.push('/contact')" type="primary" round>Get Started</el-button>
+      </div>
+
+      <!-- Mobile Menu Burger -->
+      <div class="md:hidden flex items-center">
+        <el-button @click="drawer = true" circle><el-icon>
+            <Menu />
+          </el-icon></el-button>
+      </div>
+
+    </el-row>
+
+    <el-drawer v-model="drawer" direction="rtl" size="100%" :with-header="false" append-to-body :z-index="9999">
+      <div class="p-4 flex flex-col h-full">
+
+        <el-link @click="router.push('/')" :underline="false" class="mb-4">
+          <img src="../../assets/LOGO.png" alt="Saras" style="height: 40px; width: auto;" class="p-1" />
+          <span
+            class="text-xl bg-gradient-to-r from-blue-700 to-blue-400 bg-clip-text text-transparent whitespace-nowrap">Saras
+            Finance</span>
+        </el-link>
+
+        <template v-if="!showProducts">
+          <el-menu class="el-menu-vertical-demo flex" mode="vertical" :router="false" background-color="transparent"
+            active-text-color="#409EFF">
+            <el-col>
+              <el-menu-item @click="navigateAndClose('/compliance-governance')">Compliance, Risk &
+                Governance</el-menu-item>
+              <el-menu-item @click="navigateAndClose('/audit-automation')">Audit Automation</el-menu-item>
+              <el-menu-item @click="navigateAndClose('/knowledge-advisory')">Knowledge & Advisory</el-menu-item>
+              <el-menu-item @click="navigateAndClose('/litigation-management')">Litigation Management</el-menu-item>
+            </el-col>
+          </el-menu>
         </template>
-        <template #dropdown>
-          <el-dropdown-menu>
-            <el-dropdown-item @click="router.push('/litigation-management')">Litigation Management</el-dropdown-item>
-            <el-dropdown-item @click="router.push('/compliance-governance')">Compliance & Governance</el-dropdown-item>
-            <el-dropdown-item @click="router.push('/knowledge-advisory')">Knowledge & Advisory</el-dropdown-item>
-            <el-dropdown-item @click="router.push('/audit-automation')">Audit & Due Diligence Automation</el-dropdown-item>
-          </el-dropdown-menu>
-        </template>
-      </el-dropdown>
-      <el-button @click="takeMeToApp" type="success" round>Go to Saras Portal</el-button>
-      <el-button @click="router.push('/contact')" type="primary" round>Get Started</el-button>
-    </div>
 
-  </el-row>
+        <div class="mt-auto flex flex-col ">
+          <el-button type="success" @click="gotoApp" class="mb-2">Go to Saras Portal</el-button>
+          <el-button type="primary" @click="navigateAndClose('/contact')">Get Started</el-button>
+        </div>
 
-</header>
+      </div>
+    </el-drawer>
+
+  </header>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+const showProducts = ref(false);
 
-import { useRouter } from 'vue-router';
+import { Menu } from '@element-plus/icons-vue';
 const router = useRouter();
+const drawer = ref(false);
 
-const takeMeToApp = async () => {
+
+const gotoApp = () => {
+  drawer.value = false;
   window.location.replace("https://app.sarasfinance.com/");
 }
 
+const navigateAndClose = (path) => {
+  router.push(path);
+  drawer.value = false;
+}
 </script>
 
 <style scoped>
-/* Elevate Navbar with a Soft Shadow */
 header {
   transition: box-shadow 0.2s ease-in-out;
-  /* background-color: #FFFFFFF5; */
   justify-content: space-between;
   align-items: center;
   backdrop-filter: blur(25px);
